01562d317a5173f506343373de508c9d31bb8b3d
Max Bruckner <max@maxbruckner.de>
2021-10-11 14:42:47 +0200

    Simplify standard future test

--- tests/standard_future.rs
+++ tests/standard_future.rs
@@ -1,38 +1,10 @@
 use either::Either;
 use either_future::EitherFuture;
 use futures_lite::future::block_on;
+use std::future::{ready, Ready};
 use std::marker::PhantomPinned;
 use std::pin::Pin;
 use std::task::{Context, Poll};
-
-struct ValueFuture<Type>(Option<Type>)
-where
-	Type: Unpin;
-
-impl<Type> ValueFuture<Type>
-where
-	Type: Unpin,
-{
-	fn new(value: Type) -> ValueFuture<Type> {
-		ValueFuture(Some(value))
-	}
-}
-
-impl<Type> std::future::Future for ValueFuture<Type>
-where
-	Type: Unpin,
-{
-	type Output = Type;
-
-	fn poll(self: Pin<&mut Self>, _context: &mut Context<'_>) -> Poll<Self::Output> {
-		Poll::Ready(
-			Pin::into_inner(self)
-				.0
-				.take()
-				.expect("ValueFuture has already resolved."),
-		)
-	}
-}
 
 struct NotPinFuture {
 	_phantom: PhantomPinned,
@@ -48,7 +20,7 @@ impl std::future::Future for NotPinFuture {
 
 #[test]
 fn should_run_left_future() {
-	let either = Either::<_, ValueFuture<()>>::Left(ValueFuture::new(42));
+	let either = Either::<_, Ready<i32>>::Left(ready(42));
 	let either_future = EitherFuture::from(either);
 
 	assert_eq!(Either::Left(42), block_on(either_future));
@@ -56,7 +28,7 @@ fn should_run_left_future() {
 
 #[test]
 fn should_run_right_future() {
-	let either = Either::<ValueFuture<()>, _>::Right(ValueFuture::new(42));
+	let either = Either::<Ready<i32>, _>::Right(ready(42));
 	let either_future = EitherFuture::from(either);
 
 	assert_eq!(Either::Right(42), block_on(either_future));
@@ -64,7 +36,7 @@ fn should_run_right_future() {
 
 #[test]
 fn should_work_with_unpin() {
-	let either = Either::<_, NotPinFuture>::Left(ValueFuture::new(42));
+	let either = Either::<_, NotPinFuture>::Left(ready(42));
 	let either_future = EitherFuture::from(either);
 
 	assert_eq!(Either::Left(42), block_on(either_future));
